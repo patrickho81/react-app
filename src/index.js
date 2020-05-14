be0c1a44e76872cfc778f9a94f24624660cc678d
Patrick <patrickho81@gmail.com>
2020-05-14 14:21:55 -0700

    added font-awesome, bootstrap-social for header and footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap-social/bootstrap-social.css";
 import "./index.css";
 import App from "./App";
 //import AppSample from "./AppSample";
